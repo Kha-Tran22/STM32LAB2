5d0f3f236cb14801518753f1a4bc2d49eab31c9b
Kha Tran <tuankhavntv@gmail.com>
2024-10-03 08:27:45 +0700

    no message

--- CubeIDE/Core/Inc/software_timer.h
+++ CubeIDE/Core/Inc/software_timer.h
@@ -1,17 +1,20 @@
 /*
  * software_timer.h
  *
- *  Created on: Oct 2, 2024
- *      Author: tuank
+ *  Created on: Oct 2, 2022
+ *      Author: DELL
  */
 
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
-#define TICK 10
-extern int timer_flag[10];
+extern int timer1_flag;
+extern int timer2_flag;
+extern int timer3_flag;
 
-void setTimer(int index, int duration);
+void setTimer1(int duration);
+void setTimer2(int duration);
+void setTimer3(int duration);
 void timerRun();
 
 #endif /* INC_SOFTWARE_TIMER_H_ */
