0ec1ad5ce950a256dea6abea3a0f1c2af3160ce0
Kha Tran <tuankhavntv@gmail.com>
2024-10-03 08:47:17 +0700

    Revert "no message"
    This reverts commit 5d0f3f236cb14801518753f1a4bc2d49eab31c9b.

--- CubeIDE/Core/Inc/software_timer.h
+++ CubeIDE/Core/Inc/software_timer.h
@@ -1,20 +1,17 @@
 /*
  * software_timer.h
  *
- *  Created on: Oct 2, 2022
- *      Author: DELL
+ *  Created on: Oct 2, 2024
+ *      Author: tuank
  */
 
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
-extern int timer1_flag;
-extern int timer2_flag;
-extern int timer3_flag;
+#define TICK 10
+extern int timer_flag[10];
 
-void setTimer1(int duration);
-void setTimer2(int duration);
-void setTimer3(int duration);
+void setTimer(int index, int duration);
 void timerRun();
 
 #endif /* INC_SOFTWARE_TIMER_H_ */
